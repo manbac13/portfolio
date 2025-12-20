dadaba4981deaf082ffa46268c15d646bab4e0d9
manishbachhavservicepackai <manish.b@servicepack.ai>
2025-12-20 14:05:27 +0530

    env added

--- src/components/landing/location.jsx
+++ src/components/landing/location.jsx
@@ -7,7 +7,7 @@ const Location = () => {
       <Box
         sx={{ display: "flex", justifyContent: "space-between", py: 1, px: 2 }}
       >
-        <Typography variant="body2">Pune, India</Typography>
+        <Typography variant="body2">{import.meta.env.VITE_LOCATION}</Typography>
         <Stack direction={"row"} spacing={1} alignItems={"center"}>
           <BlinkingDot />
           <Typography variant="body2">Available to work</Typography>
